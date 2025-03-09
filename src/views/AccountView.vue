ba434fefde111e1c7816ee58c455bc5e8c352567
Huy Pham <phamngohoanghuy95@gmail.com>
2025-03-09 18:34:53 +0700

    learn conditional

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,38 +1,51 @@
 <script setup>
 import { ref } from 'vue';
 
+  const categoryProduct = ref('food')
   const products = ref([
     {
       id: 1,
-      name: "Account 1",
-      title: "Software Engineer I"
+      name: "Pizza",
+      category: "Food"
     },
     {
       id: 2,
-      name: "Account 2",
-      title: "Software Engineer II"
+      name: "Tra sua",
+      category: "Drink"
     },
     {
       id: 3,
-      name: "Account 3",
-      title: "Software Engineer III"
+      name: "Com Chien",
+      category: "Food"
+    },
+    {
+      id: 4,
+      name: "Nuoc mia",
+      category: "Drink"
     },
   ])
-
+const handleCategoryProduct = (value) => {
+  categoryProduct.value = value;
+} 
 </script>
 
 
 <template>
     <div class="account">
-      <h1>This is an My account page</h1>
-      <div v-for="product in  products">
+      <h1>This is an My List page</h1>
+      <button @click="handleCategoryProduct('food')">Show Food</button>
+      <button @click="handleCategoryProduct('drink')">Show Drink</button>
+      <!-- <div v-for="product in  products">
           <div class="group-item-product">
             <div>{{ product.id }}</div>
             <div>{{ product.name}}</div>
             <div>{{ product.title }}</div>
           </div>
-      </div>
+      </div> -->
+      <div v-if="categoryProduct === 'food'">Hien thi Danh sach Food</div>
+      <div v-else>Hien thi Danh sach Drink</div>
     </div>
+
 </template>
   
   <style>
